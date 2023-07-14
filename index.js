ad1f915e4cf1be75bce36c854164bc0d679c1092
ihumph0228 <128190629+ihumph0228@users.noreply.github.com>
2023-07-14 11:01:54 -0500

    Added movies

--- index.js
+++ index.js
@@ -11,9 +11,58 @@ app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 const topMovies = [
-  // Your top 10 movies data here
-];
-
+    {
+      title: 'Iron Man',
+      year: 2008,
+      director: 'Jon Favreau'
+    },
+    {
+      title: 'Captain America: The Winter Soldier',
+      year: 2014,
+      director: 'Anthony Russo, Joe Russo'
+    },
+    {
+      title: 'Guardians of the Galaxy',
+      year: 2014,
+      director: 'James Gunn'
+    },
+    {
+      title: 'Avengers: Infinity War',
+      year: 2018,
+      director: 'Anthony Russo, Joe Russo'
+    },
+    {
+      title: 'Black Panther',
+      year: 2018,
+      director: 'Ryan Coogler'
+    },
+    {
+      title: 'Thor: Ragnarok',
+      year: 2017,
+      director: 'Taika Waititi'
+    },
+    {
+      title: 'Spider-Man: Homecoming',
+      year: 2017,
+      director: 'Jon Watts'
+    },
+    {
+      title: 'Captain Marvel',
+      year: 2019,
+      director: 'Anna Boden, Ryan Fleck'
+    },
+    {
+      title: 'Doctor Strange',
+      year: 2016,
+      director: 'Scott Derrickson'
+    },
+    {
+      title: 'Avengers: Endgame',
+      year: 2019,
+      director: 'Anthony Russo, Joe Russo'
+    }
+  ];
+  
 // GET route for /movies
 app.get('/movies', (req, res) => {
   res.json(topMovies);
